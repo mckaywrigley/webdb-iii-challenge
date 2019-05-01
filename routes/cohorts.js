17ac609d2051b0a39d4935122ad579b92fb56352
Mckay <mckaywrigley@gmail.com>
2019-05-01 12:44:23 -0700

    GET cohorts

--- routes/cohorts.js
+++ routes/cohorts.js
@@ -10,17 +10,29 @@ const db = knex(knexConfig);
 router.post("/", (req, res) => {
   const { name } = req.body;
   if (!name) {
-    res.status(400).json({ error: "Please provide a name for the cohort." });
+    return res
+      .status(400)
+      .json({ error: "Please provide a name for the cohort." });
   }
   db("cohorts")
     .insert(req.body)
     .then(cohort => {
-      res.status(201).json(cohort);
+      return res.status(201).json(cohort);
     })
     .catch(err => {
-      res
+      return res
         .status(400)
         .json({ error: "The cohort could not be saved to the database." });
+    });
+});
+
+router.get("/", (req, res) => {
+  db("cohorts")
+    .then(cohorts => {
+      return res.status(200).json(cohorts);
+    })
+    .catch(err => {
+      return res.status(404).json({ error: "Cohorts could not be found." });
     });
 });
 
